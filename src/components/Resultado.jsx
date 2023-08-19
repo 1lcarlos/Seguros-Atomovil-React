2e971888e6188dd320a0e742bd1348eaebb8838f
lcarlos <luiscardozoicg@gmail.com>
2023-08-19 00:16:05 -0500

    Add Hooks useCallback and useMemo

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,3 +1,4 @@
+import { useCallback, useMemo, useRef } from "react";
 import useCotizador from "../hooks/useCotizador";
 import { MARCAS, PLANES } from "../constans";
 
@@ -5,8 +6,23 @@ const Resultado = () => {
   const { resultado, datos } = useCotizador();
   const { marca, year, plan } = datos;
   if (resultado === 0) return null;
-  const [nommbreMarca] = MARCAS.filter((m) => m.id === Number(marca));
-  const [nommbrePlan] = PLANES.filter((p) => p.id === Number(plan));
+  /* const [nommbreMarca] = useCallback(
+    MARCAS.filter((m) => m.id === Number(marca)),
+    [resultado]
+  ); */
+   const [nommbreMarca] = useMemo( () =>
+    MARCAS.filter((m) => m.id === Number(marca)),
+    [resultado]
+  );
+ /*  const [nommbrePlan] = useCallback(
+    PLANES.filter((p) => p.id === Number(plan)),
+    [resultado]
+  ); */
+  const [nommbrePlan] = useMemo( ()=>
+    PLANES.filter((p) => p.id === Number(plan)),
+    [resultado]
+  );
+  const yearRef = useRef(year);
 
   return (
     <div className=" text-center bg-gray-100 mt-5 p-5 shadow">
@@ -22,7 +38,7 @@ const Resultado = () => {
       </p>
       <p className=" my-2">
         <span className=" font-bold">Modelo del Auto: </span>
-        {year}
+        {yearRef.current}
       </p>
       <p className=" my-2 text-2xl">
         <span className=" font-bold">Total Cotización: {resultado} </span>
